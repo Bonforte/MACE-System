b1b9a5571128619e1b597488935cef4a89df6900
Bonforte <gnitescu4@gmail.com>
2021-07-26 11:20:49 +0300

    solved hvmonitoring issues

--- HVMON/HVMonInflux.py
+++ HVMON/HVMonInflux.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import time
 from influxdb import InfluxDBClient
 from epics import caget
+import xml.etree.ElementTree as ET
 
 #Declaring some constants that we will use:
 epicsaddress='9b0ab43a3f7d7ff0'
@@ -19,32 +20,46 @@ client=InfluxDBClient(host=dbip,port=dbport,database=indb)
 clientDet=InfluxDBClient(host=dbip,port=dbport,database=indb)
 
 #Seeing what is active using configuration file
-conf_list=[]
-conf_ch=[]
-conf_slots=[]
-with open('HVConfiguration.conf','r') as f:
-    line_data=f.readline()
-    conf_list=line_data.split(',')
+slotvec=[]
+channelsvec=[]
+xmltree=ET.parse('/home/eliade/MAC-System-Grafana/HVMON/HVConfiguration.xml')
+xmlroot=xmltree.getroot()
+for x in xmlroot.findall('board'):
+    slot=x.find('slot').text
+    slot=int(slot)
+    channels=x.find('chno').text
+    channels=int(channels)
+    slotvec.append(slot)
+    channelsvec.append(channels)
 
-#Converting vector strings to integers to be used
-for i in range(len(conf_slots)):
-    conf_list[i]=int(conf_list[i])
+#Deleting 0 values for better efficiency of alarm trigger
+channelsvecdel=[]
+slotvecdel=[]
+for i in range(len(channelsvec)):
+    if channelsvec[i]==0:
+        channelsvecdel.append(i)
+cntr=0
+for ch in channelsvecdel:
+    channelsvec.remove(channelsvec[ch-cntr])
+    cntr=cntr+1
 
-#Dividing numbers from configuration list into slots and channels:
-for i in range(0,len(conf_list),2):
-    conf_slots.append(int(conf_list[i]))
-    conf_ch.append(int(conf_list[i+1]))
+for x in range(len(slotvec)):
+    if slotvec[x]==0:
+        slotvecdel.append(x)
+cntr=0
+for sl in slotvecdel:
+    slotvec.remove(slotvec[sl-cntr])
+    cntr=cntr+1
+print(channelsvec,slotvec)
 
-print(conf_slots)
-print(conf_ch)
 #Printing working slots for insight:
 print("\n\nActive slots with respective channels:\n")
-for i in range(0,len(conf_slots)):
-    print("Slot "+str(conf_slots[i])+" with the number of channels being: "+str(conf_ch[i])+";")
+for i in range(0,len(slotvec)):
+    print("Slot "+str(slotvec[i])+" with the number of channels being: "+str(channelsvec[i])+";")
     print("-------------------------------------------")
 
 #Reading waiting time between read-outs:
-with open('/GUI/wait_time.txt','r') as f:
+with open('/home/eliade/MAC-System-Grafana/GUI/wait_time.txt','r') as f:
     wtime=int(f.read())
 print("\nThe waiting time between read-outs is : "+ str(wtime)+" seconds.")
 
@@ -55,34 +70,13 @@ resultI=[]
 #Run the monitoring:
 while(1):
     timeset=datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-    for j in range(len(conf_slots)):
-        for x in range(conf_ch[j]):
-            if x>=10:
-                if conf_slots[j]>=10:
-                    m1=caget(epicsaddress+':'+str(conf_slots[j])+':0'+str(x)+':VMon')
-                else:
-                    m1=caget(epicsaddress+':'+'0'+str(conf_slots[j])+':0'+str(x)+':VMon')
-            else:
-                if conf_slots[j]>=10:
-                    m1=caget(epicsaddress+':'+str(conf_slots[j])+':00'+str(x)+':VMon')
-                else:
-                    m1=caget(epicsaddress+':'+'0'+str(conf_slots[j])+':00'+str(x)+':VMon')
+    for j in range(len(slotvec)):
+        for x in range(channelsvec[j]):
+            m1=caget(epicsaddress+':'+str(slotvec[j]).zfill(2).zfill(2)+':'+str(x).zfill(3)+':VMon')
             resultV.append(m1)
-            if x>=10:
-                if conf_slots[j]>=10:
-                    m2=caget(epicsaddress+':'+str(conf_slots[j])+':0'+str(x)+':IMon')
-                else:
-                    m2=caget(epicsaddress+':'+'0'+str(conf_slots[j])+':0'+str(x)+':IMon')
-            else:
-                if conf_slots[j]>=10:
-                    m2=caget(epicsaddress+':'+str(conf_slots[j])+':00'+str(x)+':IMon')
-                else:
-                    m2=caget(epicsaddress+':'+'0'+str(conf_slots[j])+':00'+str(x)+':IMon')
+            m2=caget(epicsaddress+':'+str(slotvec[j]).zfill(2)+':'+str(x).zfill(3)+':IMon')
             resultI.append(m2)
-        if conf_slots[j]>=10:
-            m3=caget(epicsaddress+':'+str(conf_slots[j])+':Temp')
-        else:
-            m3=caget(epicsaddress+':'+'0'+str(conf_slots[j])+':Temp')
+        m3=caget(epicsaddress+':'+str(slotvec[j]).zfill(2)+':Temp')
         resultT=m3
         json_bodyboard=[
             {
@@ -90,19 +84,18 @@ while(1):
             "time":timeset,
             "fields":
             {
-                "temp"+str(conf_slots[j]):resultT
+                "temp"+str(slotvec[j]):resultT
             }
             }
         ]
-        client.write_points(json_bodyboard)
-        for y in range(conf_ch[j]):
+        for y in range(channelsvec[j]):
             json_bodyboard.append(
                {
             "measurement": "HVMONDB",
             "time":timeset,
             "fields":
             {
-                "VMon"+str(conf_slots[j])+'|'+str(y+1):resultV[y]
+                "VMon"+str(slotvec[j])+'|'+str(y+1):resultV[y]
             }
             }
             )
@@ -112,7 +105,7 @@ while(1):
             "time":timeset,
             "fields":
             {
-                "IMon"+str(conf_slots[j])+'|'+str(y+1):resultI[y]
+                "IMon"+str(slotvec[j])+'|'+str(y+1):resultI[y]
             }
             }
             )
@@ -121,7 +114,3 @@ while(1):
         resultI=[]
         resultT=0
     time.sleep(wtime)
-
-
-
-        
