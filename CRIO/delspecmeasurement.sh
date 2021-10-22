92ccbfbf9e2bcb3a7cafe8f4af08474e3daad626
GNTechELI <george.nitescu@eli-np.ro>
2021-10-22 12:11:12 +0300

    first commit

--- CRIO/delspecmeasurement.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Please enter the name of the table you want to delete"
-
-read tablename1
-
-
-
-influx -database 'CRIO' -host 172.18.4.156 -port 8086 -execute "drop measurement $tablename1"
--- CRIO/seeactivemeasurements.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-influx -database 'CRIO' -host 172.18.4.156 -port 8086 -execute "show measurements"
